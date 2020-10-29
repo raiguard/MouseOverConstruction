fef19faa7812dd124ac5f96115c6a30ec3a9b203
raiguard <caleb.heuer@gmail.com>
2020-10-28 18:26:22 -0600

    Remove unused map setting

--- src/settings.lua
+++ src/settings.lua
@@ -1,13 +1,6 @@
 data:extend{
   {
     type = "bool-setting",
-    name = "moc-instant-deconstruction",
-    setting_type = "runtime-global",
-    default_value = false,
-    order = "a"
-  },
-  {
-    type = "bool-setting",
     name = "moc-enable-construction",
     setting_type = "runtime-per-user",
     default_value = true,
