1e3f6f16fa7ceb315322752e91278d857d4e6847
raiguard <caleb.heuer@gmail.com>
2020-11-22 21:42:01 -0700

    Update to Factorio 1.1

--- src/control.lua
+++ src/control.lua
@@ -141,12 +141,10 @@ event.register("moc-toggle", function(e)
   local player = game.get_player(e.player_index)
   local player_table = global.players[e.player_index]
   player_data.toggle_mouseover(player, player_table)
-  -- when 1.1 comes out
-  -- player.create_local_flying_text{
-  --   text = {"moc-message."..(player_table.flags.mouseover_enabled and "enabled" or "disabled").."-moc"},
-  --   create_at_cursor = true
-  -- }
-  player.print{"moc-message."..(player_table.flags.mouseover_enabled and "enabled" or "disabled").."-moc"}
+  player.create_local_flying_text{
+    text = {"moc-message."..(player_table.flags.mouseover_enabled and "enabled" or "disabled").."-moc"},
+    create_at_cursor = true
+  }
 end)
 
 -- ENTITY
