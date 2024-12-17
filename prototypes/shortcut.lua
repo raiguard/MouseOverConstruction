9b59ae399475c23bdf5e58d6357820362defcfdb
Caleb Heuer <ch@raiguard.me>
2024-12-16 17:45:58 -0700

    Update shortcut icons

--- prototypes/shortcut.lua
+++ prototypes/shortcut.lua
@@ -2,8 +2,9 @@ data:extend({
   {
     type = "shortcut",
     name = "moc-toggle",
+    -- FIXME: New 56-pixel icon
     icon = "__MouseOverConstruction__/graphics/shortcut-x32.png",
-    icon_size = 32,
+    icon_size = 36,
     small_icon = "__MouseOverConstruction__/graphics/shortcut-x24.png",
     small_icon_size = 24,
     action = "lua",
